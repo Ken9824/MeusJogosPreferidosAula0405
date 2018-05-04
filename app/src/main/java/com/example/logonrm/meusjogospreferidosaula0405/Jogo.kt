39ba43acba1652b955e604b766fd1782f7b67bc6
logonrm <logonrm@fiap.com.br>
2018-05-04 09:43:18 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrm/meusjogospreferidosaula0405/Jogo.kt
@@ -0,0 +1,6 @@
+package com.example.logonrm.meusjogospreferidosaula0405
+
+data class Jogo(val imagemId: Int,
+                val titulo: String,
+                val anoLancamento: Int,
+                val descricao: String)
